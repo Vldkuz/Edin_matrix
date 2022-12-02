e83ec75098c9f48dc94b076e5a3a5b3a57e83c8b
Vladislav <kuznetsov230304@gmail.com>
2022-12-02 22:00:23 +0500

    Добавьте файлы проекта.

---
+++ Edin_matrix/Edin_matrix.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    double** matrix = new double* [N];
+    for (int i = 0; i < N; ++i)
+        matrix[i] = new double[N] {0};
+
+    for (int i = 0; i < N; i++)
+        matrix[i][i] = 1;
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
